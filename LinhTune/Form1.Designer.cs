c9c976ebd2d02e8a50c67e6a8e89a0f29f10753b
linh <caolinh14789@gmail.com>
2025-09-21 01:11:21 +0700

    first time

---
+++ LinhTune/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace LinhTune
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            SuspendLayout();
+            // 
+            // guna2Panel1
+            // 
+            guna2Panel1.BackColor = SystemColors.ActiveCaptionText;
+            guna2Panel1.CustomizableEdges = customizableEdges1;
+            guna2Panel1.Location = new Point(34, 26);
+            guna2Panel1.Name = "guna2Panel1";
+            guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            guna2Panel1.Size = new Size(90, 89);
+            guna2Panel1.TabIndex = 0;
+            // 
+            // guna2Panel2
+            // 
+            guna2Panel2.BackColor = SystemColors.ActiveCaptionText;
+            guna2Panel2.BorderRadius = 30;
+            guna2Panel2.BorderStyle = System.Drawing.Drawing2D.DashStyle.DashDot;
+            guna2Panel2.CustomizableEdges = customizableEdges3;
+            guna2Panel2.Dock = DockStyle.Bottom;
+            guna2Panel2.Location = new Point(0, 151);
+            guna2Panel2.Name = "guna2Panel2";
+            guna2Panel2.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            guna2Panel2.Size = new Size(523, 307);
+            guna2Panel2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(523, 458);
+            Controls.Add(guna2Panel2);
+            Controls.Add(guna2Panel1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
+    }
+}
---
+++ LinhTune/Form1.cs
@@ -0,0 +1,10 @@
+namespace LinhTune
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
